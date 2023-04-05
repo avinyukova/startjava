3d20464824fffa4c527800d67ae59419bd1d89a2
av.vinyukova <avinyukova@bk.ru>
2023-04-05 17:49:01 +0300

    Добавлен файл VariablesTheme.java
    - ДЗ Переменные: примитивные типы данных

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,6 +1,6 @@
-public class MyFirstApp {
+
+public class MyFirstApp {	
     public static void main(String[] args) {
-        System.out.print("Написано однажды, ");
-        System.out.println("работает везде!");
+        System.out.println("Write once, run anywhere");
     }
 }
---
+++ VariablesTheme.java
@@ -0,0 +1,144 @@
+public class VariablesTheme {   
+
+    public static void main(String[] args) {
+        System.out.println("1 Вывод значений переменных на консоль");
+
+        byte name = 5;
+        short name_1 = 32500;
+        int name_2 = 23123123;
+        long name_3 = 1231923213;
+        float name_4 = 32.14f;
+        double name_5 = 33.1515d;
+        char name_6 = 'A';
+        boolean name_7 = false;
+        System.out.print(name);
+        System.out.println(" - Целое число");
+        System.out.print(name_1);
+        System.out.println(" - Целое число");
+        System.out.print(name_2);
+        System.out.println(" - Целое число");
+        System.out.print(name_3);t
+        System.out.println(" - Целое число");
+        System.out.print(name_4);
+        System.out.println(" - Вещественное число");
+        System.out.print(name_5);
+        System.out.println(" - Вещественное число, двойной точности");
+        System.out.print(name_6);
+        System.out.println(" - Символ");
+        System.out.print(name_7);
+        System.out.println(" - Логическое значение\n");
+        
+        System.out.println("2 Расчет стоимости товара со скидкой\n");
+        int item1 = 200, item2 = 100;
+        int sales = item1 + item2;
+        System.out.println(sales);
+        int sales1 = sales / 100 * 11;
+        System.out.println(sales1);
+        int sales2 = sales - sales1;
+        System.out.println(sales2);
+        
+        System.out.println("3 Вывод слова JAVA");
+        System.out.println("   J     a   v     v   a");
+        System.out.println("   J    a a   v   v   a a");
+        System.out.println("J  J   aaaaa   V V   aaaaa");
+        System.out.println(" JJ   a     a   V   a     a\n");
+        
+        System.out.println("4 Вывод min и max значений целых числовых типов");
+        byte byt = 5;
+        System.out.println(byt);
+        byt++;
+        System.out.println(byt);
+        byt--;
+        System.out.println(byt);
+        short sho = 42;
+        System.out.println(sho);
+        sho++;
+        System.out.println(sho);
+        sho--;
+        System.out.println(sho);
+        int in = 12_345;
+        System.out.println(in);
+        in++;
+        System.out.println(in);
+        in--;
+        System.out.println(in);
+        long lon = 1_122_334_445;
+        System.out.println(lon);
+        lon++;
+        System.out.println(lon);
+        lon--;
+        System.out.println(lon);
+        
+        System.out.println("5 Перестановка значений переменных");
+        System.out.println("#1. с помощью третьей переменной");
+        int digi1 = 2;
+        int digi2 = 5;
+        System.out.println(digi1);
+        System.out.println(digi2);
+        System.out.printf("digi1 = %s digi2 = %s\n", digi1, digi2);
+        int temp;
+        temp = digi2;
+        digi2 = digi1;
+        digi1 = temp;
+        System.out.printf("digi1 = %s digi2 = %s\n", digi1, digi2);
+        System.out.println("#.2 с помощью арифметических операций");
+        digi2 = digi2 + digi1;
+        digi1 = digi2 - digi1;
+        digi2 = digi2 - digi1;
+        System.out.printf("digi1 = %s digi2 = %s\n", digi1, digi2);
+        digi2 = digi2 + digi1;
+        digi1 = digi2 - digi1;
+        digi2 = digi2 - digi1;
+        System.out.printf("digi1 = %s digi2 = %s\n", digi1, digi2);
+        System.out.println("#.3 с помощью побитовой операции");
+        digi2 = digi2^digi1;
+        digi1 = digi2^digi1;
+        digi2 = digi2^digi1; 
+        System.out.printf("digi1 = %s digi2 = %s\n", digi1, digi2);
+        digi2 = digi2^digi1;
+        digi1 = digi2^digi1;
+        digi2 = digi2^digi1;
+        System.out.printf("digi1 = %s digi2 = %s\n", digi1, digi2);
+        
+        System.out.println("6 Вывод символов и их кодов");
+        char ch1 = '#';
+        char ch2 = '&';
+        char ch3 = '@';
+        char ch4 = '^';
+        char ch5 = '_';
+        int ch6 = 35;
+        int ch7 = 38;
+        int ch8 = 64;
+        int ch9 = 94;
+        int ch10 = 95;
+        System.out.printf("%c %s\n %c %s\n %c %s\n %c %s\n %c %s\n", ch1, ch6, ch2, ch7, ch3, ch8, ch4, ch9, ch5, ch10);
+        
+        System.out.println("7 Вывод в консоль ASCII-арт Дюка");
+        char ch11 = 47;
+        char ch12 = 92;
+        char ch13 = 95;
+        char ch14 = 40;
+        char ch15 = 41;
+        System.out.printf("     %c%c     \n    %c  %c    \n   %c%c%c %c%c \n  %c      %c \n %c%c%c%c%c%c%c%c%c%c \n", ch11, ch12, ch11, ch12, ch11, ch13, ch14, ch15, ch12, ch11, ch12, ch11, ch13, ch13, ch13, ch13, ch11, ch12, ch13, ch13, ch12);
+        
+        System.out.println("8 Вывод количества сотен, десятков и единиц числа");
+        int number = 123;
+        int num1 = (number/100);
+        int num2 = (number/10);
+        int num3 = (number/1);
+        System.out.printf("Число 123 содержит:\n %d сотню\n %d десятков\n %d единицы\n", num1, num2, num3);
+        int digit1 = 1; 
+        int digit2 = 2;
+        int digit3 = 3; 
+        int sim = digit1 + digit2 + digit3;
+        System.out.printf("Сумма его цифр: %d\n", sim);
+        int sim1 = digit1 * digit2 * digit3;
+        System.out.printf("Произведение: %d\n", sim1);
+        
+        System.out.println("9 Вывод времени");
+        int time = (86399 / 3600) % 24;
+        int min = (86399 / 60) % 60;
+        int sec = 86399 % 60;
+        System.out.printf(" ЧЧ%d:MM%d:CC%d\n", time, min, sec);
+    }
+}
